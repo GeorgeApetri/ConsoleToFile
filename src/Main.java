3077c460db3e5c5853282a72be666d3b7111943f
George <apetrigeorge@yahoo.com>
2019-03-04 18:34:12 +0200

    add console reader

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@
 public class Main {
 
     public static void main(String[] args) {
+
         ToFile toFile = new ToFile();
-        toFile.setInput("Hello PentaStagiu! :)");
-        toFile.writeToFile();
+        toFile.consoleToFile();
     }
 }
--- src/ToFile.java
+++ src/ToFile.java
@@ -1,24 +1,37 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ToFile {
 
-    public String input;
 
-    public void writeToFile() {
-        Path path = Paths.get("c:/output.txt");
+    public void consoleToFile() {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while (true) {
+            System.out.print("Input: ('quit' for exit) ");
+            input = null;
+            try {
+                input = bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (input.equalsIgnoreCase("quit")) {
+                System.out.println("Exit!");
+                break;
+            }
+            Path path = Paths.get("c:/output.txt");
 
-        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
-            writer.write(input);
-        } catch (IOException e) {
-            e.printStackTrace();
+            try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+                writer.write(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }
 
-    public void setInput(String input) {
-        this.input = input;
     }
 }
